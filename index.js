74fb85b7e7b2daf15f6bb33de9908363d1d73cac
unknown <aniketshetyeajs@gmail.com>
2020-11-09 12:25:03 +0530

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bot = new Client()
 bot.on('ready', () => {console.log('bot live....')})
 
 bot.on('message', (message) => {
-   //reply etc goes here 
+   if(message.content.length > 0){
+        setTimeout(() => message.channel.send("hey guys"), 5000)
+   } 
 })
 
 bot.login(process.env.DISCORDJS_BOT)
